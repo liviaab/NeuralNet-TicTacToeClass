af6af70ca07161c7365a57b4e3be70c6d8dd03bf
liviaab <liviaab7@gmail.com>
2016-11-23 20:27:26 -0200

    Commit inicial

---
+++ changeinput.py
@@ -0,0 +1,135 @@
+import sys
+import random
+
+# from changeinp import *
+# change_ttt("Bases/tic-tac-toe.data", "ttt.data")
+def change_ttt(entrada, saida):
+	# x ->	-0.5	o -> 0.5		b -> 0
+	# positivo -> 1
+	# negativo -> 0
+	entrada = open(entrada, "r")
+	saida = open(saida, "w")
+
+	for line in entrada:
+		line = line.split(",")
+
+		if line[-1].rstrip() == "positive":
+			line[-1] = '1'
+		else:
+			line[-1] = '0'
+
+		for i in range(0,9) :
+			aux = ''
+			if line[i] == 'x':
+				aux = -0.5
+			elif line[i] == 'o':
+				aux = 0.5
+			else:
+				aux = 0
+			saida.write(str(aux) + "," )
+
+
+		saida.write(line[-1]+ "\n")
+
+	entrada.close()
+	saida.close()
+
+"""
+# change_car("Bases/car.data", "car2.data")
+def change_car(entrada, saida):
+	# unacc -> 0	acc -> 1
+	# good -> 2		vgood -> 3
+	# MUDARR
+
+	entrada = open(entrada, "r")
+	saida = open(saida, "w")
+
+	for line in entrada:
+		line = line.split(",")
+
+		if line[-1] == "unacc\n":
+			line[-1] = '0'
+		elif line[-1] == "acc\n":
+			line[-1] = '1'
+		elif line[-1] == "good\n":
+			line[-1] = '2'
+		else:
+			line[-1] = '3'
+
+		for i in range(0,10) :
+			saida.write(line[i])
+			if not i == 9:
+				saida.write(",")
+			else:
+				saida.write("\n")
+
+	entrada.close()
+	saida.close()
+"""
+
+# ti, tt, vi, vt = separate_trainvalidtn_set("ttt.data")
+def separate_trainvalidtn_set(entrada):
+	entrada = open(entrada, "r")
+
+	data = []
+	train = []
+	traintarget = []
+	validation = []
+	validationtarget = []
+
+	for line in entrada:
+		data.append(line)
+
+	random.shuffle(data)
+
+	for line in data:
+		line = line.split(",")
+
+		if len(train) >= len(data)*0.7:
+			validationtarget.append([ float( line[-1].rstrip() ) ])
+			a = []
+			for i in range(0,9):
+				a.append(float(line[i]))
+			validation.append(a)
+
+		else:
+			traintarget.append([float(line[-1].rstrip() )])
+			a = []
+			for i in range(0,9):
+				a.append(float(line[i]))
+			train.append(a)
+
+
+	print "Tamanho treino: ", len(train), "Tamanho Validacao: ",  len(validation), "Total: ", len(train)+len(validation)
+
+	train_file = open("tr_inp.data", 'w')
+	for i in train:
+		for k in range(0, 9):
+			if k == 8:
+				train_file.write(str(i[k])+"\n")
+			else:
+				train_file.write(str(i[k])+",")
+	train_file.close()
+
+	traintarget_file = open("tr_targ.data", 'w')
+	for i in traintarget:
+		traintarget_file.write(str(i[0])+"\n")
+	traintarget_file.close()
+
+	validation_file = open("vl_inp.data", 'w')
+	for i in validation:
+		for k in range(0, 9):
+			if k == 8:
+				validation_file.write(str(i[k])+"\n")
+			else:
+				validation_file.write(str(i[k])+",")
+	validation_file.close()
+
+	validtarget_file = open("vl_targ.data", 'w')
+	for i in validationtarget:
+		validtarget_file.write(str(i[0])+"\n")
+	validtarget_file.close()
+
+
+change_ttt("Bases/tic-tac-toe.data", temparq)
+separate_trainvalidtn_set("ttt.data")
---
+++ ttt.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+# python ttt.py "Bases/tic-tac-toe.data"
+import sys
+import time
+import pprint
+import neurolab as nl
+import numpy as np
+import pylab as pl
+from changeinput import *
+
+# create neural net with 3 inputs - input range [-0.5, 0.5]
+# 5 neuronios  para a camada escondida, 1 para a saída
+# 2 layers including hidden layer and output layer
+net = nl.net.newff([[-0.5, 0.5], [-0.5, 0.5], [-0.5, 0.5],[-0.5, 0.5], [-0.5, 0.5], [-0.5, 0.5],[-0.5, 0.5], [-0.5, 0.5], [-0.5, 0.5]], [5, 1])
+print "Net1 num neuronios entrada", net.ci
+print "Net1 num neuronios saida", net.co
+print "Net1 num camadas escondida+Saida", len(net.layers)
+print ""
+# create neural net with 5 inputs - input range [-0.5, 0.5]
+# 5 neuronios  para as duas camadas escondidas, 1 para a saída
+# 3 layers including hidden layer and output layer
+# net2 = nl.net.newff([[-0.5, 0.5], [-0.5, 0.5], [-0.5, 0.5], [-0.5, 0.5], [-0.5, 0.5],[-0.5, 0.5], [-0.5, 0.5], [-0.5, 0.5], [-0.5, 0.5]], [5, 5, 1])
+# print "Net2 num neuronios entrada", net2.ci
+# print "Net2 num neuronios saida", net2.co
+# print "Net2 num camadas escondida+Saida", len(net2.layers)
+
+
+# Train networks
+#net.trainf = nl.train.train_gd()
+tr_inp = open("tr_inp.data", 'r')
+tr_targ = open("tr_targ.data", 'r')
+vl_inp = open("vl_inp.data", 'r')
+
+ti = []
+for line in tr_inp:
+    line = line.rstrip()
+    line = line.split(",")
+    aux = []
+    for k in line:
+        aux.append( float(k) )
+    ti.append(aux)
+
+tt = []
+for line in tr_targ:
+    tt.append([float(line.strip())])
+
+print"Treinando a rede"
+error = net.train(ti, tt, epochs=500, show=100, goal=0.001)
+pprint.pprint(error)
+print len(error)
+#error2 = net2.train(tr_inp, tr_targ, epochs=500, show=1, goal=0.02)
+
+print "Erro 1"
+# pprint.pprint(error)
+
+# print 'Erro 2'
+#pprint.pprint(error2)
+
+# Simulate networks
+#out = net.sim(val_inp)
+#out2 = net2.sim(val_inp)
+#print "OUT 1"
+#pprint.pprint(out)
+
+# Plot results
+pl.subplot(111)
+pl.plot(error)
+pl.xlabel('Epoch number')
+pl.ylabel('error (default SSE)')
+pl.show()
+
+# pl.subplot(222)
+# pl.plot(error2)
+# pl.xlabel('Epoch number')
+# pl.ylabel('error (default SSE)')
+# pl.show()
