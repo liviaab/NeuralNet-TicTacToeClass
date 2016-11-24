c25c92ec970a2b5f165f1dec3bab7180f0f6c80d
liviaab <liviaab7@gmail.com>
2016-11-23 23:49:06 -0200

    Tratamento segundo dataset

--- changeinput.py
+++ changeinput.py
@@ -1,10 +1,8 @@
 import sys
 import random
 
-# from changeinp import *
-# change_ttt("Bases/tic-tac-toe.data", "ttt.data")
 def change_ttt(entrada, saida):
-	# x ->	-0.5	o -> 0.5		b -> 0
+	# x ->	-0.5		o -> 0.5		b -> 0
 	# positivo -> 1
 	# negativo -> 0
 	entrada = open(entrada, "r")
@@ -34,41 +32,72 @@ def change_ttt(entrada, saida):
 	entrada.close()
 	saida.close()
 
-"""
-# change_car("Bases/car.data", "car2.data")
+def findclass4opt(tipo):
+	if tipo == "vhigh" or tipo == '2'or "unacc":
+		a = 0
+		b = 0
+	elif tipo == "high"or tipo == '3' or "acc":
+		a = 0
+		b = 1
+	elif tipo == "med"or tipo == '4' or "good":
+		a = 1
+		b = 0
+	else: #low ou 5more ou vgood
+		a = 1
+		b = 1
+
+	return a, b
+
+def findclass3opt(tipo):
+	if tipo == "2" or tipo == "small" or tipo == "low":
+		a = -1
+	elif tipo == "4" or tipo == "med":
+		a = 0
+	else: #more, big ou high
+		a = 1
+	return a
+
+
 def change_car(entrada, saida):
-	# unacc -> 0	acc -> 1
-	# good -> 2		vgood -> 3
-	# MUDARR
+	"""
+	1 buying	vhigh, high, med, low 		00,01,10,11
+	2 maint		vhigh, high, med, low 		00,01,10,11
+	3 doors		2, 3, 4, 5more 			00,01,10,11
+	4 persons	2, 4, more 			-1,0,1
+	5 lug-boot	small, med, big 		-1,0,1
+	6 safety	low, med, high 			-1,0,1
+	7 Class		unacc, acc, good, vgood 	00,01,10,11
 
-	entrada = open(entrada, "r")
-	saida = open(saida, "w")
-
+	"""
+	entrada = open(entrada, 'r')
+	saida = open(saida, 'w')
+	
 	for line in entrada:
+		line = line.rstrip()
 		line = line.split(",")
-
-		if line[-1] == "unacc\n":
-			line[-1] = '0'
-		elif line[-1] == "acc\n":
-			line[-1] = '1'
-		elif line[-1] == "good\n":
-			line[-1] = '2'
-		else:
-			line[-1] = '3'
-
-		for i in range(0,10) :
-			saida.write(line[i])
-			if not i == 9:
-				saida.write(",")
-			else:
-				saida.write("\n")
+		#1
+		a, b = findclass4opt(line[0])
+		#2
+		c, d = findclass4opt(line[1]) 
+		#3
+		e, f = findclass4opt(line[2]) 
+		#4
+		g = findclass3opt(line[3])
+		#5
+		h = findclass3opt(line[4]) 
+		#6
+		i = findclass3opt(line[5]) 
+		#7
+		j, k = findclass4opt(line[6])
+		
+		saida.write(str(a)+','+str(b)+','+str(c)+','+str(d)+','+str(e)+','+str(f)+','+str(g)+','+str(h)+','+str(i)+','+str(j)+','+str(k)+'\n')
 
 	entrada.close()
 	saida.close()
-"""
+
 
 # ti, tt, vi, vt = separate_trainvalidtn_set("ttt.data")
-def separate_trainvalidtn_set(entrada):
+def separate_trvl_lists(entrada, tam):
 	entrada = open(entrada, "r")
 
 	data = []
@@ -88,19 +117,23 @@ def separate_trainvalidtn_set(entrada):
 		if len(train) >= len(data)*0.7:
 			validationtarget.append([ float( line[-1].rstrip() ) ])
 			a = []
-			for i in range(0,9):
+			for i in range(0,tam):
 				a.append(float(line[i]))
 			validation.append(a)
 
 		else:
 			traintarget.append([float(line[-1].rstrip() )])
 			a = []
-			for i in range(0,9):
+			for i in range(0,tam):
 				a.append(float(line[i]))
 			train.append(a)
 
-
 	print "Tamanho treino: ", len(train), "Tamanho Validacao: ",  len(validation), "Total: ", len(train)+len(validation)
+	entrada.close()
+	return train, traintarget, validation, validationtarget 
+
+def separate_trainvalidtn_settt(entrada):
+	train, traintarget, validation, validationtarget  = separate_trvl_lists(entrada, 9)
 
 	train_file = open("tr_inp.data", 'w')
 	for i in train:
@@ -130,6 +163,39 @@ def separate_trainvalidtn_set(entrada):
 		validtarget_file.write(str(i[0])+"\n")
 	validtarget_file.close()
 
+def separate_sets_car(entrada):
+	train, traintarget, validation, validationtarget  = separate_trvl_lists(entrada, 11)
+
+	train_file = open("ctr_inp.data", 'w')
+	for i in train:
+		for k in range(0, 11):
+			if k == 10:
+				train_file.write(str(i[k])+"\n")
+			else:
+				train_file.write(str(i[k])+",")
+	train_file.close()
+
+	traintarget_file = open("ctr_targ.data", 'w')
+	for i in traintarget:
+		traintarget_file.write(str(i[0])+"\n")
+	traintarget_file.close()
+
+	validation_file = open("cvl_inp.data", 'w')
+	for i in validation:
+		for k in range(0, 11):
+			if k == 10:
+				validation_file.write(str(i[k])+"\n")
+			else:
+				validation_file.write(str(i[k])+",")
+	validation_file.close()
+
+	validtarget_file = open("cvl_targ.data", 'w')
+	for i in validationtarget:
+		validtarget_file.write(str(i[0])+"\n")
+	validtarget_file.close()	
+		
 
 change_ttt("Bases/tic-tac-toe.data", "ttt.data")
-separate_trainvalidtn_set("ttt.data")
+separate_trainvalidtn_settt("ttt.data")
+change_car("Bases/car.data", "car.data")
+separate_sets_car("car.data")
--- ttt.py
+++ ttt.py
@@ -10,22 +10,13 @@ import pylab as pl
 # create neural net with 3 inputs - input range [-0.5, 0.5]
 # 5 neuronios  para a camada escondida, 1 para a saída
 # 2 layers including hidden layer and output layer
-net = nl.net.newff([[-0.5, 0.5], [-0.5, 0.5], [-0.5, 0.5],[-0.5, 0.5], [-0.5, 0.5], [-0.5, 0.5],[-0.5, 0.5], [-0.5, 0.5], [-0.5, 0.5]], [5, 1])
-print "Net1 num neuronios entrada", net.ci
-print "Net1 num neuronios saida", net.co
-print "Net1 num camadas escondida+Saida", len(net.layers)
+net = nl.net.newff([[-0.5, 0.5], [-0.5, 0.5], [-0.5, 0.5],[-0.5, 0.5], [-0.5, 0.5], [-0.5, 0.5],[-0.5, 0.5], [-0.5, 0.5], [-0.5, 0.5]], [9, 1])
+print "Neuronios entrada", net.ci
+print "Neuronios saida", net.co
+print "Numero camadas escondida+Saida", len(net.layers)
 print ""
-# create neural net with 5 inputs - input range [-0.5, 0.5]
-# 5 neuronios  para as duas camadas escondidas, 1 para a saída
-# 3 layers including hidden layer and output layer
-# net2 = nl.net.newff([[-0.5, 0.5], [-0.5, 0.5], [-0.5, 0.5], [-0.5, 0.5], [-0.5, 0.5],[-0.5, 0.5], [-0.5, 0.5], [-0.5, 0.5], [-0.5, 0.5]], [5, 5, 1])
-# print "Net2 num neuronios entrada", net2.ci
-# print "Net2 num neuronios saida", net2.co
-# print "Net2 num camadas escondida+Saida", len(net2.layers)
 
 
-# Train networks
-#net.trainf = nl.train.train_gd()
 tr_inp = open("tr_inp.data", 'r')
 tr_targ = open("tr_targ.data", 'r')
 vl_inp = open("vl_inp.data", 'r')
@@ -43,33 +34,33 @@ tt = []
 for line in tr_targ:
     tt.append([float(line.strip())])
 
-print"Treinando a rede. Goal: 0.001"
+# Train networks
+#net.trainf = nl.train.train_cg
+
+print"Treinando redes. Goal: 0.001"
 error = net.train(ti, tt, epochs=500, show=10, goal=0.001)
-pprint.pprint(error)
 print len(error)
-#error2 = net2.train(tr_inp, tr_targ, epochs=500, show=1, goal=0.02)
 
-print "Erro 1"
-# pprint.pprint(error)
-
-# print 'Erro 2'
-#pprint.pprint(error2)
+vl = []
+for line in vl_inp:
+    line = line.rstrip()
+    line = line.split(",")
+    aux = []
+    for k in line:
+        aux.append( float(k) )
+    vl.append(aux)
 
 # Simulate networks
-#out = net.sim(val_inp)
-#out2 = net2.sim(val_inp)
-#print "OUT 1"
-#pprint.pprint(out)
+out = net.sim(vl)
+
+print "Saida"
+for i in out:
+	print int(i)
 
 # Plot results
-pl.subplot(111)
+pl.subplot(221)
 pl.plot(error)
 pl.xlabel('Epoch number')
 pl.ylabel('error (default SSE)')
 pl.show()
 
-# pl.subplot(222)
-# pl.plot(error2)
-# pl.xlabel('Epoch number')
-# pl.ylabel('error (default SSE)')
-# pl.show()
